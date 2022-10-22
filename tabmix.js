51b0922baedfb8ecaab70aac90ab912d45136e95
ot07 <ot07neoki@gmail.com>
2022-10-23 03:51:46 +0900

    update tabmix.js

--- tabmix.js
+++ tabmix.js
@@ -1,10 +1,12 @@
-// space
+// space (size 4)
     const a = 0;
+// space (size 8)
+        const b = 0;
 // tab
-		const b = 0;
+		const c = 0;
 // tabmix and incorrect indent
-	 const c = 0;
+	 const d = 0;
 // tabmix
-	  const d = 0;
-//comment tabmix
+	        const e = 0;
+// comment tabmix
 	  // comment
